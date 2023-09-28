3a95315f9b2bd1e2f2f1eb8a7a29d0de7fc3829c
jaimegarciasdr9 <jaimegarciasdr9@gmail.com>
2023-09-28 19:46:01 +0200

    New commits for my repository in github

--- setup.py
+++ setup.py
@@ -1,42 +1,21 @@
 from setuptools import setup, find_packages
+from module_structure import __author__,__version__,__name__
 
-# Project metadata
-name = "example-project"
-version = "0.1.0"
-description = "An example Python project"
-author = "Your Name"
-author_email = "your.email@example.com"
-url = "https://github.com/yourusername/example-project"
-
-# Required packages (dependencies)
-install_requires = [
-    # List your project's dependencies here
-]
-
-# Entry points (if your project includes scripts or console entry points)
-entry_points = {
-    'console_scripts': [
-        'my_script = example_project.module:main_function',
-    ],
-}
-
-# Define the package structure and files to include
-packages = find_packages()
-
-# Additional package data (e.g., non-Python files)
-package_data = {
-    'example_project': ['data/*.txt'],
-}
+VERSION = __version__
+AUTHOR = __author__
+NAME = __name__
 
 # Main setup configuration
 setup(
-    name=name,
-    version=version,
-    description=description,
-    author=author,
-    author_email=author_email,
-    url=url,
-    packages=packages,
+    name = "module_structure",
+    version = "0.1.0",
+    description = "My first professional project",
+    author = "Jaime García",
+    author_email = "jaimegarciasdr9@gmail.com",
+    url = "https://github.com/jaimegarciasdr9/module_structure",
+    
+    packages = find_packages,
+    include_package_data = True,
     package_data=package_data,
     install_requires=install_requires,
     entry_points=entry_points,
@@ -46,3 +25,15 @@ setup(
         "Operating System :: OS Independent",
     ],
 )
+
+
+# Required packages (dependencies)
+install_requires = [
+    # List your project's dependencies here
+]
+
+}
+
+
+
+
